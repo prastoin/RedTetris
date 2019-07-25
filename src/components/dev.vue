4aa46b0b9f6ff4095a83a44e71700e1bd7e4096f
Tkesray <45004772+Tkesray@users.noreply.github.com>
2019-07-26 00:38:46 +0200

    rotation done

--- src/components/dev.vue
+++ src/components/dev.vue
@@ -1,8 +1,21 @@
 <template>
-    <div class="root">
+    <div class="root" @click="incrRota(1)">
         <p style="font-size: 3rem">| DEV |</p>
         <div class="container">
-            
+            <div class="tetrimino">
+                <div
+                v-for="y in matriceSize"
+                :key="y"
+                style="display: flex; flex-direction: colunm;">
+                    <div
+                    v-for="x in matriceSize"
+                    :key="x"
+                    class="cell"
+                    :style="isCell(y - 1, x - 1) === true ? 'background: yellow;' : ''"
+                    >
+                    </div>
+                </div> 
+            </div>
         </div>
     </div>
 </template>
@@ -14,28 +27,28 @@ const tetrimino = [
         name: 'I',
         coord: [
             [
-                0, 0, 0, 0,
-                1, 1, 1, 1,
-                0, 0, 0, 0,
-                0, 0, 0, 0
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
             ],
             [
-                0, 0, 1, 0,
-                0, 0, 1, 0,
-                0, 0, 1, 0,
-                0, 0, 1, 0
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0]
             ],
             [
-                0, 0, 0, 0,
-                0, 0, 0, 0,
-                1, 1, 1, 1,
-                0, 0, 0, 0
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0]
             ],
             [
-                0, 1, 0, 0,
-                0, 1, 0, 0,
-                0, 1, 0, 0,
-                0, 1, 0, 0
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
             ]
         ]
     },
@@ -43,28 +56,28 @@ const tetrimino = [
         name: 'O',
         coord: [
             [
-                0, 1, 1, 0,
-                0, 1, 1, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
             ],
             [
-                0, 1, 1, 0,
-                0, 1, 1, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
             ],
             [
-                0, 1, 1, 0,
-                0, 1, 1, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
             ],
             [
-                0, 1, 1, 0,
-                0, 1, 1, 0,
-                0, 0, 0, 0,
-                0, 0, 0, 0
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
             ]
         ]
     },
@@ -72,24 +85,24 @@ const tetrimino = [
         name: 'T',
         coord: [
             [
-                1, 1, 1,
-                0, 1, 0,
-                0, 0, 0
+                [1, 1, 1],
+                [0, 1, 0],
+                [0, 0, 0]
             ],
             [
-                0, 1, 0,
-                1, 1, 1,
-                0, 0, 0
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 0, 0]
             ],
             [
-                0, 1, 0,
-                1, 1, 0,
-                0, 1, 0
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 1, 0]
             ],
             [   
-                0, 1, 0,
-                0, 1, 1,
-                0, 1, 0
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 1, 0]
             ],
         ]
     },
@@ -97,24 +110,24 @@ const tetrimino = [
         name: 'L',
         coord: [
             [
-                0, 0, 1,
-                1, 1, 1,
-                0, 0, 0
+                [0, 0, 1],
+                [1, 1, 1],
+                [0, 0, 0]
             ],
             [
-                0, 1, 0,
-                0, 1, 0,
-                0, 1, 1
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 1]
             ],
             [
-                0, 0, 0,
-                1, 1, 1,
-                1, 0, 0
+                [0, 0, 0],
+                [1, 1, 1],
+                [1, 0, 0]
             ],
             [   
-                1, 1, 0,
-                0, 1, 0,
-                0, 1, 0
+                [1, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0]
             ],
         ]
     },
@@ -122,24 +135,24 @@ const tetrimino = [
         name: 'J',
         coord: [
             [
-                1, 0, 0,
-                1, 1, 1,
-                0, 0, 0
+                [1, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0]
             ],
             [
-                0, 1, 1,
-                0, 1, 0,
-                0, 1, 0
+                [0, 1, 1],
+                [0, 1, 0],
+                [0, 1, 0]
             ],
             [
-                0, 0, 0,
-                1, 1, 1,
-                0, 0, 1
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 1]
             ],
             [   
-                0, 1, 0,
-                0, 1, 0,
-                1, 1, 0
+                [0, 1, 0],
+                [0, 1, 0],
+                [1, 1, 0]
             ]
         ]
     },
@@ -147,24 +160,24 @@ const tetrimino = [
         name: 'Z',
         coord: [
             [
-                1, 1, 0,
-                0, 1, 1,
-                0, 0, 0
+                [1, 1, 0],
+                [0, 1, 1],
+                [0, 0, 0]
             ],
             [
-                0, 0, 1,
-                0, 1, 1,
-                0, 1, 0
+                [0, 0, 1],
+                [0, 1, 1],
+                [0, 1, 0]
             ],
             [
-                0, 0, 0,
-                1, 1, 0,
-                0, 1, 1
+                [0, 0, 0],
+                [1, 1, 0],
+                [0, 1, 1]
             ],
             [   
-                0, 1, 0,
-                1, 1, 0,
-                1, 0, 0
+                [0, 1, 0],
+                [1, 1, 0],
+                [1, 0, 0]
             ]
         ]
     },
@@ -172,24 +185,24 @@ const tetrimino = [
         name: 'S',
         coord: [
             [
-                0, 1, 1,
-                1, 1, 0,
-                0, 0, 0
+                [0, 1, 1],
+                [1, 1, 0],
+                [0, 0, 0]
             ],
             [
-                0, 1, 0,
-                0, 1, 1,
-                0, 0, 1
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 0, 1]
             ],
             [
-                0, 0, 0,
-                0, 1, 1,
-                1, 1, 0
+                [0, 0, 0],
+                [0, 1, 1],
+                [1, 1, 0]
             ],
             [   
-                1, 0, 0,
-                1, 1, 0,
-                0, 1, 0
+                [1, 0, 0],
+                [1, 1, 0],
+                [0, 1, 0]
             ]
         ]
     }
@@ -198,7 +211,32 @@ const tetrimino = [
 export default {
     name: 'dev',
     data () {
-        return {tetrimino}
+        return {
+            currTetri: Math.floor(Math.random() * Math.floor(7)),
+            currRota: Math.floor(Math.random() * Math.floor(4)),
+            tetrimino,
+            x: 0,
+            }
+    },
+    methods: {
+        isCell (y, x) {
+            console.log(y, x);
+            console.log(this.tetrimino[this.currTetri].coord[this.currRota])
+            if (this.tetrimino[this.currTetri].coord[this.currRota][y][x] === 1)
+              return true;
+            console.log('nope');
+            return (false);
+        },
+        incrRota (value)
+        {
+            this.currRota++;
+            this.currRota = (this.currRota === 4 ? 0 : this.currRota);
+        }
+    },
+    computed: {
+        matriceSize() {
+            return ((this.tetrimino[this.currTetri].name === 'I' || this.tetrimino[this.currTetri].name === 'O') ? 4 : 3)
+        },
     }
 }
 </script>
@@ -216,5 +254,17 @@ export default {
     height: 880px;
     position relative;
     margin: auto;
+}
+
+.tetrimino {
+    width: 160px;
+    height: 160px;
+    position: relative;
+}
+
+.cell {
+    border: 1px solid black;
+    height: 40px;
+    width: 40px;
 }
 </style>
