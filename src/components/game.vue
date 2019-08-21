b1ad139d649aeb9e16d53879de8b0637f01e0589
Paul RASTOIN <prastoin@student.42.fr>
2019-08-21 12:46:31 +0200

    adding score + loose overlay | have to print previ

--- src/components/game.vue
+++ src/components/game.vue
@@ -26,6 +26,13 @@
                 </div>
             </div>
         </div>
+        <div v-show="currTetri.playing === false" class="loose">
+            <div class="stats">
+                <span>SUPER REDTETRIS<br></span>
+                <span>{{score}} Points !<br></span>
+                <span>| Play again |</span>
+            </div>
+        </div>
     </div>    
 </template>
 
@@ -63,10 +70,8 @@ export default {
                 this.goTo(1, 0);
             else if (key === 'ArrowLeft')
                 this.goTo(-1, 0);
-            console.log(event);
         },
         pickPrint () {
-     //       console.log('pickprint' )
             if (currTetri.playing === true)
             {
                 this.currTetri.n = this.getRandomInt(7);
@@ -75,11 +80,21 @@ export default {
                 this.currTetri.rota= this.getRandomInt(4);
                 this.currTetri.x = this.getRandomInt(10);
                 } while ((this.moveAble(0, 0, this.currTetri)) === false)
-                this.draw(this.currTetri);
+                this.render(this.currTetri);
             }
         },
+        printPre(currTetri){
+            console.log(currTetri);
+            let saveTetri = { ...currTetri };
+            console.log(saveTetri);
+            saveTetri.y = saveTetri.y >= 4 ? saveTetri.y : 4; 
+            while(this.moveAble(0, -1, saveTetri) && saveTetri.y <= 25)
+                saveTetri.y++;
+            if (saveTetri.y === 26)
+                saveTetri.y--;
+            this.draw(saveTetri);
+        },
         draw(currTetri) {
-      //      console.log('draw');
             for (let y = 0; y < 4; y++)
                 for (let x = 0; x < 4; x++)
                     if (this.board[currTetri.y + y][currTetri.x + x] != 2 && this.tetrimino[currTetri.n].coord[currTetri.rota][y][x] === 1)
@@ -87,12 +102,8 @@ export default {
             return (true);
         },
         goTo (valueX, valueY) {
-       //     console.log(`goTo = ` + valueX + valueY);
             if (this.currTetri.playing === false)
-            {
                 clearInterval(this.intervalID);
-                console.log('loose');
-            }
             if (this.moveAble(valueX, valueY, this.currTetri) === true)
             {
                 this.clear4x4(this.currTetri.y, this.currTetri.x, 0); //1 to 0
@@ -100,59 +111,56 @@ export default {
                     this.currTetri.y += valueY;
                 else
                     this.currTetri.x += valueX;
-                this.draw(this.currTetri);
+                this.render(this.currTetri);
             }
         },
+        render () {
+//           this.printPre(this.currTetri);
+           this.draw(this.currTetri);
+        },
         moveAble (valueX, valueY, currTetri) {
-          //  console.log('moveable');
             for (let y = 0; y < 4; y++)
                 for (let x = 0; x < 4; x++)
                     if (this.tetrimino[currTetri.n].coord[currTetri.rota][y][x] === 1)
-                        if ((this.valid(y + currTetri.y + valueY, currTetri.x + valueX + x, valueX)) === false)
+                        if ((this.valid(y + currTetri.y + valueY, currTetri.x + valueX + x, valueX, currTetri)) === false)
                              return (false);
             return (true);
         },
-        valid(y, x, valueX) {
+        valid(y, x, valueX, currTetri) {
             if (x < 0 || x >= 10)
                 return (false);
             if (y < 0 || y >= 22 + 4 || this.board[y][x] === 2)
             {
-                if (valueX != 0 || this.currTetri.inRota === true) 
+                if (valueX != 0 || currTetri.inRota === true) 
                     return (false);
                 if (currTetri.y >= 0 && currTetri.y <= 3)
                 {
-                    this.currTetri.playing = false;
+                    currTetri.playing = false;
                     return (false);
                 }
-                this.blockTetri(this.currTetri);
+                this.blockTetri(currTetri);
                 this.pickPrint();
                 return (false);
             }
             return (true);
         },
         blockTetri (currTetri) {
-//            console.log('blocktetri');
             this.clear4x4(currTetri.y, currTetri.x, 2); // 1 to 2
             this.fullLine(currTetri);//checking full line on 4y
         },
         fullLine(currTetri){
-//            console.log('fullLINE');
             for (let y = 0; y < 4; y++)
                 for (let x = 0; x < 10 && y < 26 && (this.board[y + currTetri.y][x] === 2); x++)
                     if (x === 9 )
                     {
- //                       console.log('deleting', y);
                         this.board[y + currTetri.y].forEach((x, index) => {
                             this.$set(this.board[y + currTetri.y], index, 0);
                         });
-//                        debugger;
                         this.fall(y + currTetri.y);
+                        this.score += 10;
                     }
         },
         fall(y){
-   //         console.log(y);
-     //       console.log(this.board);
-//            debugger;
             while (y >= 4)
             {
                 for(let x = 0; x < 10; x++)
@@ -172,7 +180,6 @@ export default {
                         this.$set(this.board[j + y], i + x, value);
         },
         incrRota() {
-       //     console.log('incrRota');
             let save = this.currTetri.rota;
             this.currTetri.rota++;
             this.currTetri.rota = (this.currTetri.rota === 4 ? 0 : this.currTetri.rota);
@@ -185,7 +192,7 @@ export default {
             }
             this.currTetri.inRota = false;
             this.clear4x4(this.currTetri.y, this.currTetri.x, 0); // 1 to 0
-            this.draw(this.currTetri);
+            this.render(this.currTetri);
         },
         getRandomInt(max) {
             return (Math.floor(Math.random() * Math.floor(max)));
@@ -253,6 +260,23 @@ export default {
             position: relative;
             text-align: left;
         }
+    }
+}
+
+.loose {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height:100%;
+    background: rgba(33, 47, 61, 0.8);
+    .stats {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: white;
+        color: red;
     }
 }
 
